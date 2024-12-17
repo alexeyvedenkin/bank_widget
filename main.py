de529d1cb12317fed549e8ac189ff8283445f456
AlexeyVedenkin <alexey.vedenkin@mail.ru>
2024-12-17 21:31:13 +0500

    correcting pyproject.toml

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from masks import get_mask_account, get_mask_card_number
+from src.masks.masks import *
 
 print(get_mask_account('12345678901234567890'))
 print(get_mask_card_number('1234567890123456'))
