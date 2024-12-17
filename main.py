f6543633d6865957aeb23139e722327d8d7265fa
AlexeyVedenkin <alexey.vedenkin@mail.ru>
2024-12-18 01:48:09 +0500

    fixed widget.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from src import masks, widget
 
 print(masks.get_mask_account('12345678901234567890'))
 print(masks.get_mask_card_number('1234567890123456'))
-print(widget.mask_account_card('Maestro 7000792289606361'))
+print(widget.mask_account_card('Счет 73654108430135874305'))
--- src/widget.py
+++ src/widget.py
@@ -1,11 +1,14 @@
 from typing import Union
-from src.masks import *
+from src import masks
 
-def mask_account_card(official_row: Union[str]) -> Union[str]:
+
+def mask_account_card(full_row: Union[str]) -> Union[str]:
     """Возвращает строку по маске для счета или карты
     """
-    if official_row[-17] == " ":
-        result = official_row.replace(official_row[-16:], get_mask_card_number(official_row[-16]))
+    if full_row[-17] == " ":
+        mask = masks.get_mask_card_number(full_row[-16:])
+        result = full_row.replace(full_row[-16:], mask)
     else:
-        result = official_row.replace(official_row[-20:], get_mask_account(official_row[-20]))
+        mask = masks.get_mask_account(full_row[-20:])
+        result = full_row.replace(full_row[-20:], mask)
     return result
