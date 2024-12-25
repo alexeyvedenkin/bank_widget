e8e27cb2a7adcedda24adaf9c1f2479a66ff68fc
AlexeyVedenkin <alexey.vedenkin@mail.ru>
2024-12-25 19:27:58 +0500

    modified filter_by_state in processing.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from src import masks, processing, widget
+from tests.test_dict import test
 
 
 print(masks.get_mask_account('12345678901234567890'))
 print(masks.get_mask_card_number('1234567890123456'))
 print(widget.mask_account_card('Счет 73654108430135874305'))
 print(widget.get_date('2024-03-1198695985929'))
-print(processing.filter_by_state([{'state': 'EXECUTED'}]))
+print(processing.filter_by_state(test))
--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,10 @@
 from typing import Union
 
+from tests.test_dict import test
+
 
 def filter_by_state(work_dict: Union[list[dict]]) -> Union[list[dict]]:
-    """Что-то возвращает
+    """Возвращает список словарей, отфильтрованный по полю 'state'
     """
     result = []
     for subject in work_dict:
@@ -10,9 +12,15 @@ def filter_by_state(work_dict: Union[list[dict]]) -> Union[list[dict]]:
             result.append(subject)
 
     return result
-    
 
 
+def sort_by_date(work_dict: Union[list[dict]]) -> Union[list[dict]]:
+    """Возвращает список словарей, упорядоченный по дате
+    в порядке убывания
+    """
+    result = []
+    return result
 
 
+print(filter_by_state(test))
 
