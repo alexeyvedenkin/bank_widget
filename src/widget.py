abed3dc49b2bf8a6d827954ba176a312c7124c11
AlexeyVedenkin <alexey.vedenkin@mail.ru>
2025-01-11 20:24:21 +0500

    изменен принцип работы функции mask_account_card

--- src/widget.py
+++ src/widget.py
@@ -6,12 +6,15 @@ from src import masks
 def mask_account_card(full_row: Union[str]) -> Union[str]:
     """Возвращает строку по маске для счета или карты
     """
-    if not full_row[-17].isdigit():
-        mask = masks.get_mask_card_number(full_row[-16:])
-        result = full_row.replace(full_row[-16:], mask)
+    work_row = full_row.split()
+    if work_row[0] in ['счет', 'расчетный', 'account']:
+        mask = masks.get_mask_card_number(work_row[-1])
+        work_row[-1] = mask
+        result = ' '.join(work_row)
     else:
-        mask = masks.get_mask_account(full_row[-20:])
-        result = full_row.replace(full_row[-20:], mask)
+        mask = masks.get_mask_account(work_row[-1])
+        work_row[-1] = mask
+        result = ' '.join(work_row)
     return result
 
 
