e9020ee81bac696e422988cb1b0cd75e75e634d9
AlexeyVedenkin <alexey.vedenkin@mail.ru>
2025-01-11 20:47:42 +0500

    modified tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,4 +3,22 @@ import pytest
 
 @pytest.fixture
 def test_for_masks():
-    return input()
+    return ['Maestro 1596837868705199',
+            'Счет 64686473678894779589',
+            'MasterCard 7158300734726758',
+            'Счет 35383033474447895560',
+            'Visa Classic 6831982476737658',
+            'Visa Platinum 8990922113665229',
+            'Visa Gold 5999414228426353',
+            'Счет 73654108430135874305]'
+            ]
+
+
+@pytest.fixture
+def test_for_state_and_date():
+    return [
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    ]
