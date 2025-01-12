eeee2699bfa13b2401dcdf70c688f00f41c482f3
AlexeyVedenkin <alexey.vedenkin@mail.ru>
2025-01-12 23:35:14 +0500

    fixed masks.py

--- src/masks.py
+++ src/masks.py
@@ -4,7 +4,6 @@ from typing import Union
 def get_mask_card_number(card_number: Union[int, str]) -> Union[str]:
     """Возвращает маску номера по правилу 'XXXX XX** **** XXXX'
         """
-    print(card_number)
     if len(str(card_number)) == 16:
         mcn = str(card_number)
         if mcn.isdigit():
@@ -19,6 +18,12 @@ def get_mask_card_number(card_number: Union[int, str]) -> Union[str]:
 def get_mask_account(account: Union[int, str]) -> Union[str]:
     """Возвращает маску номера счета по правилу '**XXXX'
         """
-    ma = str(account)
-    mask_account = "**" + ma[-4:]
-    return mask_account
+    if len(str(account)) == 20:
+        ma = str(account)
+        if ma.isdigit():
+            mask_account = "**" + ma[-4:]
+            return mask_account
+        else:
+            raise TypeError()
+    else:
+        raise ValueError()
