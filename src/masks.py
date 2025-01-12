efc225c823ef10959ed27abda01c2106080d2575
AlexeyVedenkin <alexey.vedenkin@mail.ru>
2025-01-12 23:14:17 +0500

    modified:   src/masks.py

--- src/masks.py
+++ src/masks.py
@@ -4,9 +4,16 @@ from typing import Union
 def get_mask_card_number(card_number: Union[int, str]) -> Union[str]:
     """Возвращает маску номера по правилу 'XXXX XX** **** XXXX'
         """
-    mcn = str(card_number)
-    mask_card_number = mcn[:4] + " " + mcn[4:6] + "** **** " + mcn[-4:]
-    return mask_card_number
+    print(card_number)
+    if len(str(card_number)) == 16:
+        mcn = str(card_number)
+        if mcn.isdigit():
+            mask_card_number = mcn[:4] + " " + mcn[4:6] + "** **** " + mcn[-4:]
+            return mask_card_number
+        else:
+            raise TypeError()
+    else:
+        raise ValueError()
 
 
 def get_mask_account(account: Union[int, str]) -> Union[str]:
