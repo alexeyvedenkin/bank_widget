285fecb21a5baac9105c2ee813a3c12092e3a71e
AlexeyVedenkin <alexey.vedenkin@mail.ru>
2025-01-12 23:15:25 +0500

    modified:   src/widget.py

--- src/widget.py
+++ src/widget.py
@@ -6,22 +6,28 @@ from src import masks
 def mask_account_card(full_row: Union[str]) -> Union[str]:
     """Возвращает строку по маске для счета или карты
     """
-    work_row = full_row.lower().replace('ё', 'е').split()
-    if work_row[0] not in ['счет', 'расчетный', 'account']:
-        mask = masks.get_mask_card_number(work_row[-1])
-        work_row[-1] = mask
-        result = ' '.join(work_row).title()
+    if len(full_row.split()) > 1:
+        work_row = full_row.lower().replace('ё', 'е').split()
+        if work_row[0] not in ['счет', 'расчетный', 'account']:
+            mask = masks.get_mask_card_number(work_row[-1])
+            work_row[-1] = mask
+            result = ' '.join(work_row).title()
+        else:
+            mask = masks.get_mask_account(work_row[-1])
+            work_row[-1] = mask
+            result = ' '.join(work_row).title()
+        return result
     else:
-        mask = masks.get_mask_account(work_row[-1])
-        work_row[-1] = mask
-        result = ' '.join(work_row).title()
-    return result
+        raise ValueError()
 
 
 def get_date(full_date: Union[str]) -> Union[str]:
     """Возвращает дату в формате ДД.ММ.ГГГГ
     """
-    work_date = full_date[:10].split('-')
-    normal_date = '.'.join(reversed(work_date))
+    if full_date.isdigit():
+        raise TypeError()
+    else:
+        work_date = full_date[:10].split('-')
+        normal_date = '.'.join(reversed(work_date))
 
-    return normal_date
+        return normal_date
