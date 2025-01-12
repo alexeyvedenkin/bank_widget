2206ef7981cb29ae5f3b201022accad8e9548b08
AlexeyVedenkin <alexey.vedenkin@mail.ru>
2025-01-12 23:16:12 +0500

    modified:   src/try_data.py

--- src/try_data.py
+++ src/try_data.py
@@ -7,11 +7,11 @@ data_for_masks = [
     'Visa Platinum 8990922113665229',
     'Visa Gold 5999414228426353',
     'Счет 73654108430135874305'
-    ]
+]
 
 data_for_state_and_date = [
     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-    ]
+]
