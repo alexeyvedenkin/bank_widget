7a67a77474ec1dad948f63b45547d75e9b3621f1
AlexeyVedenkin <alexey.vedenkin@mail.ru>
2025-02-03 00:48:10 +0500

    add tests/test_decorators.py

---
+++ tests/test_decorators.py
@@ -0,0 +1,67 @@
+import pytest
+import time
+from functools import wraps
+
+from _pytest.capture import CaptureResult
+
+from src.decorators import my_function
+from typing import Any, Callable, Optional, Union
+
+
+def log(filename: Optional[str] = None) -> Callable:
+    """
+    Декоратор log, который будет автоматически логировать начало и конец выполнения функции,
+    а также ее результаты или возникшие ошибки
+    """
+
+    def my_decorator(func: Any) -> Any:
+        @wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            start_time = time.time()
+            try:
+                result = func(*args, **kwargs)
+                end_time = time.time()
+                log_message = (
+                    f"{func.__name__} started at {start_time} and finished at {end_time} with result: {result}"
+                )
+                if filename is not None:
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(f"{func.__name__} ok\n")
+                else:
+                    print(log_message)
+
+            except Exception as e:
+                log_message_1 = f"{func.__name__} error {e.__class__.__name__}. Inputs: {args}, {kwargs}"
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(log_message_1 + "\n")
+                else:
+                    print(log_message_1)
+                raise e
+            return result
+
+        return wrapper
+
+    return my_decorator
+
+
+@log(filename="mylog.txt")
+def my_function(x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
+    """
+    Получение результат суммирования двух чисел
+    """
+    return x + y
+
+
+my_function(1, 2)
+
+
+def test_my_function(capsys):
+    """
+    Тест проверяет, что `TypeError` действительно выбрасывается,
+    и что в выводе содержится ожидаемое сообщение об ошибке
+    """
+    with pytest.raises(TypeError):
+        my_function(2, "3")
+    captured = capsys.readouterr()
+    assert "my_function error TypeError" in CaptureResult(out='', err='').out
