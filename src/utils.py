94cbb094cb6ec4045bd1e7c1ac25307267ca6855
AlexeyVedenkin <alexey.vedenkin@mail.ru>
2025-02-04 23:19:56 +0500

    modified:   .flake8

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,16 @@
 import json
+import os
+from typing import Any, Dict, List
 
 
-with open('data.json') as f:
-    data = json.load(f)
+def get_financial_transactions(path: str) -> List[Dict[str, Any]]:
+    if not os.path.exists(path):
+        return []
+    with open(path, encoding="utf-8") as file_json:
+        data_json = json.load(file_json)
+    return data_json
+
+
+transactions = get_financial_transactions("C:\\Users\\Макс\\my_prj\\bank widget\\data\\operations.json")
+
+# print(transactions)
